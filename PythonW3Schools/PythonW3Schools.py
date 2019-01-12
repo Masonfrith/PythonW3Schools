650064ba7faad20fd3fa7aaa9f77943aceee917e
Masonfrith <gedsaro@gmail.com>
2019-01-11 19:52:05 -0600

    add scientific number printing to see what it looks like, depending on size with show base number, or will keep e's at a certain point.

--- PythonW3Schools/PythonW3Schools.py
+++ PythonW3Schools/PythonW3Schools.py
@@ -20,3 +20,18 @@ y = 10
 print(x+y)
 z = x+y
 print(z)
+
+#test scientific numbers with e and printing.
+print()
+print("Testing scientific numbers and printing to see what looks like")
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print("x assigned 35e3 and prints...")
+print(x)
+print("y assigned 12E4 and prints...")
+print(y)
+print("z assigned -87.7e100 and prints...")
+print(z)
