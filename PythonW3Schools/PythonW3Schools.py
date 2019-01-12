7413facfc8baf2ed62ad26749339a2bed7730baf
Masonfrith <gedsaro@gmail.com>
2019-01-11 19:46:39 -0600

    Added comments on variable names add play with x and y and z and printing and adding

--- PythonW3Schools/PythonW3Schools.py
+++ PythonW3Schools/PythonW3Schools.py
@@ -8,3 +8,15 @@ print(x)
 x = "Sally" #now x is a String
 print(x)
 #yup it works
+"""
+Variables MUST start with a letter or the underscorce _ char
+cannot start with a number 1234var (NO!)
+can only be letters, _ and numbers (nums after ex: ha123ha is good)
+CASE SENSITIVE 
+"""
+
+x = 5
+y = 10
+print(x+y)
+z = x+y
+print(z)
