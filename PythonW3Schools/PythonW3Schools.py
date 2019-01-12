84739919d5a2425b4ff8855f2760f556edc84b48
Masonfrith <gedsaro@gmail.com>
2019-01-11 19:41:23 -0600

    Added type changing x variable testing for int and string and printing out

--- PythonW3Schools/PythonW3Schools.py
+++ PythonW3Schools/PythonW3Schools.py
@@ -2,3 +2,9 @@
 """ Testing a longer comment
 that is multi line. """
 print("Hello world.")
+
+x = 4 # x should be an int for now.
+print(x)
+x = "Sally" #now x is a String
+print(x)
+#yup it works
